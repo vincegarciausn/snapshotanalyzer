dd97854feaa7d01fe07f629695a93a944a718d38
vincegarciausn <vincegarcia05@outlook.com>
2019-09-26 00:51:43 -0700

    added the start and stop of instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -6,22 +6,72 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
+def filter_instances(project):
+    instances = []
 
-def list_instances():
+    if project:
+         filters = [{'Name':'tag:Project', 'Values':[project]}]
+         instances = ec2.instances.filter (Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+
+@instances.command('list')
+@click.option('--project', default=None, help="Only instances for project 9tag Project:<name>)")
+
+def list_instances(project):
     "List EC2 instances"
-    for i in ec2.instances.all():
+     
+    instances = filter_instances(project) 
+
+    for i in instances:
+        tags = {t['Key']: t['Value'] for t in i.tags or []}    #declaring a dictionary t...get all tags for an instance
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('Project', '<no project>'))))
     return
 
 
+@instances.command('stop')
+@click.option('--project', default=None, help='Only instances for project')
+
+def stop_instances(project):
+    "Stop EC2 instances"
+    
+    instances = filter_instances(project) 
+
+    for i in instances:
+        print ("stopping {0}...".format(i.id))
+        i.stop()
+   
+    return    
+
+
+@instances.command('start')
+@click.option('--project', default=None, help='Only instances for project')
+
+def stop_instances(project):
+    "Start EC2 instances"
+    
+    instances = filter_instances(project) 
+
+    for i in instances:
+        print ("starting {0}...".format(i.id))
+        i.start()
+   
+    return    
+
 if __name__== '__main__':
    # print (sys.argv) 
-    list_instances()   
-    
+     instances()
 
