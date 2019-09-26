9fe6d9f032663bd2896d812f3644ba2eaf57de0c
vincegarciausn <vincegarcia05@outlook.com>
2019-09-25 23:34:51 -0700

    added click

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,27 @@
 import boto3
+#import sys  # in order to read in parameters
+import click
+
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
+
 
 if __name__== '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+   # print (sys.argv) 
+    list_instances()   
+    
 
