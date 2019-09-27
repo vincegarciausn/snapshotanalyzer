d47b8057cc0475713d7236015e9f24f678af55ea
vincegarciausn <vincegarcia05@outlook.com>
2019-09-26 21:12:57 -0700

    with basic snapshots feature

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -75,8 +75,6 @@ def list_volumes(project):
     return
 
 
-
-
 @cli.group('instances')
 def instances():
     """Commands for instances"""
@@ -88,6 +86,22 @@ def create_snapshots(project):
 
     instances = filter_instances(project) 
 
+    for i in instances:
+        print("Stopping {0}.....".format(i.id))
+        
+        i.stop()
+        i.wait_until_stopped()
+        
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by Snappy")
+            
+        print("Starting {0}.....".format(i.id))
+        
+        i.start()
+        i.wait_until_running()
+    print("Job is done!")
+    return
 
 ##START OF LIST INSTANCES
 @instances.command('list')
